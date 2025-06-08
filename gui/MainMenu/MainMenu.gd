57bb29f480bdea2d68b8a9456b89dd7c9a6fc524
necrashter <iiilker99@gmail.com>
2025-06-08 17:06:44 +0300

    Add sandbox button

--- gui/MainMenu/MainMenu.gd
+++ gui/MainMenu/MainMenu.gd
@@ -6,8 +6,12 @@ func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus.call_deferred()
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://road/main.tscn")
+	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
 
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gui/Credits/credits.tscn")
+
+
+func _on_start_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://road/main.tscn")
