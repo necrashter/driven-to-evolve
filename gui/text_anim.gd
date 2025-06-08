635ad2bfe0bc9e0003f9ea14224901a261efee2c
protam12 <omerappled@gmail.com>
2025-06-08 18:12:06 +0300

    soun

--- gui/text_anim.gd
+++ gui/text_anim.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready() -> void:
 	$AnimationPlayer.play(&"main")
+	$ObjectiveComplete.play()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == &"main":
