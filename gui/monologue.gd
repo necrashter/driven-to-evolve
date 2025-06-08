369d01a8f157d181ae5056f2ff8a4544832100f8
necrashter <iiilker99@gmail.com>
2025-06-08 19:59:31 +0300

    evolutoin

--- gui/monologue.gd
+++ gui/monologue.gd
@@ -1,1 +1,35 @@
 extends Panel
+
+var current: int = 0
+var data: Array
+var objective: ObjectiveLabel = null
+
+func _ready() -> void:
+	Global.objective_complete.connect(on_objective_complete)
+
+func prepare(new_data: Array) -> void:
+	data = new_data
+	enable_datum(data[current])
+
+func enable_datum(datum: Dictionary) -> void:
+	if datum.has("text"):
+		%RichTextLabel.text = datum["text"]
+		show()
+	elif datum.has("objective"):
+		objective = datum["objective"]
+		if objective.button_pressed:
+			next()
+
+func _on_ok_button_pressed() -> void:
+	hide()
+	next()
+
+func next():
+	current += 1
+	if current < len(data):
+		enable_datum(data[current])
+
+func on_objective_complete(completed_objective) -> void:
+	if completed_objective == objective:
+		objective = null
+		next()
--- levels/tutorial.gd
+++ levels/tutorial.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	road.segments[0].place_block(path3d.curve, road.road_width, 1)
 	road.segments[0].place_arch(path3d.curve, road.road_width, path3d.road_len - 5)
 	print("road ", path3d.road_len)
-	var dialogue = [
+	$Monologue.prepare([
 		{
 			"text": "[center][b]Welcome![/b][/center]\n" +
 			"This game simulates evolutoin.",
@@ -14,13 +14,13 @@ func _ready() -> void:
 			"text": "Complete your first objective to continue.",
 		},
 		{
-			"check": null,
+			"objective": objectives.get_child(0),
 		},
 		{
 			"text": "[center][b]Well Done![/b][/center]\n" +
 			"This game simulates evolutoin.",
 		},
-	]
+	])
 
 func reset_road() -> void:
 	pass
